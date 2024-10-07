51234e66e234b1a9fd471f0c55aed8dea60af9a9
terryllowery <tlowe33@my.wgu.edu>
2024-10-07 16:13:06 -0500

    [Sprint 1][Task 5] Fix: Setup route testing and routes without logic

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
-use axum::{routing::Router, serve::Serve};
 use tower_http::services::ServeDir;
+use axum::{
+    http::{response, StatusCode}, response::IntoResponse, routing::{get, post, Router}, serve::Serve
+};
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -10,7 +12,12 @@ pub struct Application {
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
-            .nest_service("/", ServeDir::new("assets"));
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
 
 
         let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
@@ -24,4 +31,24 @@ impl Application {
         println!("Listening on {}", self.address);
         self.server.await
     }
+}
+// TODO: Implement the signup, login, logout, verify-2fa, and verify-token handlers
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK
 }
