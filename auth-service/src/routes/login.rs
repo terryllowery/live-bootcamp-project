2da3fa289bfbd15d714db13eade405f062181dbf
terryllowery <tlowe33@my.wgu.edu>
2024-11-11 22:38:48 -0600

    [Sprint 3[Task 2] Fix: Auth token

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,39 +1,49 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 use crate::{
     app_state::AppState,
     domain::{AuthAPIError, Email, Password},
+    utils::auth::generate_auth_cookie,
 };
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     let password = match Password::parse(request.password) {
         Ok(password) => password,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+        Err(_) => return(jar, Err(AuthAPIError::InvalidCredentials)),
     };
 
     let email = match Email::parse(request.email) {
         Ok(email) => email,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
 
     let user_store = state.user_store.read().await;
 
     if user_store.validate_user(&email, &password).await.is_err() {
-        return Err(AuthAPIError::IncorrectCredentails);
+        return (jar, Err(AuthAPIError::IncorrectCredentails));
     };
 
     // TODO: Return user in the response
     #[allow(unused_variables)]
     let user = match user_store.get_user(&email).await {
         Ok(user) => user,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
 
-    Ok(StatusCode::OK.into_response())
+    let auth_cookie = match generate_auth_cookie(&user.email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(auth_cookie);
+
+   (updated_jar, Ok(StatusCode::OK.into_response()))
 }
 
 #[derive(Deserialize, Debug, Serialize)]
--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -1,1 +1,26 @@
+use dotenvy::dotenv;
+use lazy_static::lazy_static;
+use std::env as std_env;
+
 pub const JWT_COOKIE_NAME: &str = "jwt";
+
+lazy_static! {
+    pub static ref JWT_SECRET: String = set_token();
+}
+
+// This is definitely NOT a good secret. We will update it soon!
+// const JWT_SECRET: &str = "secret";
+
+fn set_token() -> String {
+    dotenv().ok();
+    let secret = std_env::var("JWT_SECRET_ENV_VAR").expect("JWT_SECRET_ENV_VAR must be set");
+
+    if secret.is_empty() {
+        panic!("JWT_SECRET_ENV_VAR must be set");
+    }
+    secret
+}
+
+pub mod env {
+    
+}
